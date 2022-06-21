ba3542a9141a0ff56a494d29d998ed9ea6515cb7
Phillip <doran.phillip.3ed@gmail.com>
2022-06-20 20:53:48 -0400

    remove env keys

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Paypal from './paypal'
+
+const GQL_SERVER = 'http://localhost:4000/gql'
 
 function App() {
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    fetchToken()
+  }, [])
+
+  const fetchToken = async () => {
+    try {
+      const response = await fetch(GQL_SERVER, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer t1'
+        },
+        body: JSON.stringify({
+          query: `mutation clientToken {
+          paypalCreateClientToken {
+              token
+          }
+        }`,
+          variables: {}
+        })
+      })
+      const data = await response.json()
+      setToken(data.data.paypalCreateClientToken.token)
+    } catch (e) {
+      console.log('error!')
+      console.log(e)
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Paypal token={token} />
       </header>
     </div>
   );
---
+++ src/paypal.js
@@ -0,0 +1,181 @@
+import { useState, useRef } from 'react'
+import { PayPalScriptProvider, PayPalHostedFieldsProvider, PayPalHostedField, usePayPalHostedFields, usePayPalScriptReducer } from '@paypal/react-paypal-js';
+
+const CLIENT_ID = process.env.REACT_APP_PAYPAL_CLIENT_ID
+const GQL_SERVER = process.env.REACT_APP_GQL_SERVER
+const TICKET_REQUEST = {
+  attributes: {
+    event: "events/d38aa50f-bc05-4429-b230-24bb0d61413d",
+    items: [{
+      ticketTierKey: "a34a4b01-597a-4b7d-b5e3-575d230fdf5a",
+      quantity: 1
+    },
+    {
+      ticketTierKey: "68c69f95-ea0c-40e9-b784-9d323c907d6f",
+      quantity: 2
+    }]
+  }
+}
+
+const Paypal = ({ token }) => {
+  if (!token) return (<div>No Token</div>)
+
+  const SubmitPayment = ({ customStyle }) => {
+    const [paying, setPaying] = useState(false);
+    const cardHolderName = useRef(null);
+    const [{ isResolved }] = usePayPalScriptReducer()
+    const hostedField = usePayPalHostedFields();
+    console.log('resolved: ', isResolved)
+
+    const handleClick = () => {
+      if (!hostedField?.cardFields) {
+        const childErrorMessage = 'Unable to find any child components in the <PayPalHostedFieldsProvider />';
+        console.log('error: ', childErrorMessage)
+      }
+      const isFormInvalid =
+        Object.values(hostedField.cardFields.getState().fields).some(
+          (field) => !field.isValid
+        ) || !cardHolderName?.current?.value;
+
+      if (isFormInvalid) {
+        return alert(
+          "The payment form is invalid"
+        );
+      }
+      setPaying(true);
+      hostedField.cardFields
+        .submit({
+          cardholderName: cardHolderName?.current?.value,
+        })
+        .then((order) => {
+          console.log('payment order :', order)
+          fetch(GQL_SERVER, {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer t1'
+            },
+            body: JSON.stringify({
+              query: `mutation paypalCapturePayment($orderId: String!) {
+                paypalCapturePayment(orderId: $orderId) {
+                  _id
+                  amount
+                  items {
+                    _id
+                    tierDetails {
+                      current
+                      price
+                    }
+                  }
+                }
+            }`,
+              variables: { "orderId": order.orderId }
+            })
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log('payment data: ', data)
+              // Inside the data you can find all the information related to the payment
+            })
+            .catch((err) => {
+              // Handle any error
+            });
+        })
+    };
+
+    return (
+      <>
+        <label title="This represents the full name as shown in the card">
+          Card Holder Name
+          <input
+            id="card-holder"
+            ref={cardHolderName}
+            className="card-field"
+            style={{ ...customStyle, outline: "none" }}
+            type="text"
+            placeholder="Full name"
+          />
+        </label>
+        <button
+          className={`btn${paying ? "" : " btn-primary"}`}
+          style={{ float: "right" }}
+          onClick={handleClick}
+        >
+          {paying ? <div className="spinner tiny" /> : "Pay"}
+        </button>
+      </>
+    );
+  }
+
+  const NotEligible = props => {
+    return <>
+      <div>
+        Not eligible!
+      </div>
+    </>
+  }
+
+  // console.log('token: ', token)
+  return (
+    <PayPalScriptProvider options={{
+      'client-id': CLIENT_ID,
+      'data-client-token': token,
+      components: 'buttons,hosted-fields',
+      intent: "capture"
+    }}>
+      <PayPalHostedFieldsProvider notEligibleError={<NotEligible />} createOrder={() => {
+        return fetch(GQL_SERVER, {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer t1'
+          },
+          body: JSON.stringify({
+            query: `mutation createOrder($attributes: PaypalCreateOrderInput!) {
+              paypalCreateOrder(attributes: $attributes) {
+                id
+              }
+            }`,
+            variables: TICKET_REQUEST
+          })
+        }).then(response => response.json()).then(order => {
+          console.log('order: ', order)
+          return order.data.paypalCreateOrder.id
+        }).catch(err => {
+          console.log('error: ', err)
+        })
+      }}>
+        <div>
+          Card Number
+        </div>
+        <PayPalHostedField
+          id="card-number"
+          hostedFieldType="number"
+          options={{ selector: "#card-number" }}
+        />
+        <div>
+          CVV
+        </div>
+        <PayPalHostedField
+          id="cvv"
+          hostedFieldType="cvv"
+          options={{ selector: "#cvv" }}
+        />
+        <div>
+          Expiration Date
+        </div>
+        <PayPalHostedField
+          id="expiration-date"
+          hostedFieldType="expirationDate"
+          options={{
+            selector: "#expiration-date",
+            placeholder: "MM/YY",
+          }}
+        />
+        <SubmitPayment />
+      </PayPalHostedFieldsProvider>
+    </PayPalScriptProvider>
+  )
+}
+
+export default Paypal
